85bbe75ff747e5d00d8ee51b38e2b88abc194711
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-05-28 18:02:20 -0500

    :bug: Modularize 13_file_io.py
    The paths were relative to where you called the file from, which led to
    weird behavior. This also modularizes the file so that it is more
    Pythonic and documents the functions used by the main function

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -4,28 +4,37 @@ at how to read and write to files here:
 
 https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 """
+
+from os.path import abspath, dirname
+
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
-# YOUR CODE HERE
 
 
 def read_and_print(file_path):
+    """Open a file print it to the console
+
+    Args:
+        file_path: String -- A path-like object pointing to the file you'd like to read
+    """
     with open(file_path, 'r') as f:
         for line in f:
             print(line)
 
-
-read_and_print('src/foo.txt')
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
-
 
 def write_lines(file_path, text_to_write):
+    """Overwrite a file with either a string or an array of strings
+
+    Args:
+        file_path: String -- A path-like object pointing to the file you'd like to write to (will create a new file if not found)
+        text_to_write: Array or String -- The text you would like written to the file found at file_path
+    """
     if isinstance(text_to_write, list) is False:
         text_to_write = text_to_write.split('\n')
 
@@ -34,18 +43,28 @@ def write_lines(file_path, text_to_write):
             f.write(f'{line}\n')
 
 
-lines = [
-    'To be, or not to be, that is the question:',
-    "Whether 'tis nobler in the mind to suffer",
-    'The slings and arrows of outrageous fortune'
-]
+def main():
+    # Default lines List
+    lines = [
+        'To be, or not to be, that is the question:',
+        "Whether 'tis nobler in the mind to suffer",
+        'The slings and arrows of outrageous fortune'
+    ]
 
-write_lines('src/bar.txt', lines)
-read_and_print('src/bar.txt')
+    # Path to the directory of the module
+    base_path = dirname(abspath(__file__))
 
-# Uncomment the following lines to see how the write_lines function
-# can handle both strings and arrays
+    # Read foo.txt (already exists)
+    read_and_print(f'{base_path}/foo.txt')
 
-# lines2 = "This works as well\nsince it gets converted\nto an array"
-# write_lines('src/baz.txt', lines2)
-# read_and_print('src/baz.txt')
+    # Create bar.txt and then read it
+    write_lines(f'{base_path}/bar.txt', lines)
+    read_and_print(f'{base_path}/bar.txt')
+
+    # OPTIONAL: see how write_lines() can handle both strings and arrays
+    # write_lines(f'{base_path}/baz.txt', lines2)
+    # read_and_print(f'{base_path}/baz.txt')
+
+
+if __name__ == '__main__':
+    main()
